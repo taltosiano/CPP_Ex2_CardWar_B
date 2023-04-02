27c92e5196f3907ad66dc8255855bbb3d39227f9
taltosiano <94299489+taltosiano@users.noreply.github.com>
2023-04-02 17:35:49 +0300

    Update card.cpp

--- sources/card.cpp
+++ sources/card.cpp
@@ -1,7 +1,14 @@
 #include "card.hpp"
+#include <iostream>
+#include <string>
 
+using namespace std;
 namespace ariel{
-Card::Card(){
+Card::Card(int number){
+    } 
 
-    }  
+int Card::getCardNum(){
+    return 0;
+} 
+
 } 
